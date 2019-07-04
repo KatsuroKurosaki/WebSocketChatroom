16f571c663ded0ab95953d6aae7aa1294b08da7c
Joaquim Sardiña <joaquimsb89@gmail.com>
2019-07-04 14:03:50 +0200

    Use a nicer headercss and footerjs, as well, add reconnectingwebsocket script

--- footerjs.php
+++ footerjs.php
@@ -1,8 +1,11 @@
 <!-- jQuery -->
-<script src="js/jquery.min-3.3.1.js?_=<?=filemtime('js/jquery.min-3.3.1.js')?>" type="text/javascript" charset="UTF-8"></script>
+<script src="js/jquery.min-3.3.1.js" type="text/javascript" charset="UTF-8"></script>
 
 <!-- Bootstrap -->
-<script src="js/bootstrap.bundle.min-4.3.1.js?_=<?=filemtime('js/bootstrap.bundle.min-4.3.1.js')?>" type="text/javascript" charset="UTF-8"></script>
+<script src="js/bootstrap.bundle.min-4.3.1.js" type="text/javascript" charset="UTF-8"></script>
+
+<!-- Reconnecting WebSocket -->
+<script src="js/reconnecting-websocket.min.js" type="text/javascript" charset="UTF-8"></script>
 
 <!-- Global functions -->
-<script src="js/common_func.js?_=<?=filemtime('js/common_func.js')?>" type="text/javascript" charset="UTF-8"></script>
+<script src="js/common_func.min.js?<?=filemtime('js/common_func.min.js')?>" type="text/javascript" charset="UTF-8"></script>
--- headercss.php
+++ headercss.php
@@ -1,2 +1,2 @@
 <!-- Bootstrap -->
-<link href="css/bootstrap.min-4.3.1.css?_=<?=filemtime('css/bootstrap.min-4.3.1.css')?>" type="text/css" rel="stylesheet" />
+<link href="css/bootstrap.min-4.3.1.css" type="text/css" rel="stylesheet" />
